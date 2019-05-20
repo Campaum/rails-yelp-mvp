1e7d773a09e1feecf138043a50fed27c2a367074
Camille Paumier <cam.paumier@gmail.com>
2019-05-20 17:16:05 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'french',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+  },
+  {
+    name:         'Pierre Sang',
+    address:      'rue Oberkampf, Paris 75011',
+    category:  'japanese',
+  },
+  {
+    name:         'Postiche',
+    address:      'rue Jean-Jacques Rousseau, Paris 75001',
+    category:  'french',
+  },
+  {
+    name:         'Septime',
+    address:      'rue de Charonne, Paris 75011',
+    category:  'french',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
